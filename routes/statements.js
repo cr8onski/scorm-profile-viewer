4fc403491f0b5e5190b9a797efd55ff2a6f5150f
tom creighton <tjcreighton@gmail.com>
2016-12-16 15:27:33 -0500

    validating initialized, terminated, suspended and resumed statements

--- routes/statements.js
+++ routes/statements.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var debug = require('debug')('scorm-profile-viewer:statements');
+var validate = require('jsonschema').validate;
+var schemas = {
+    "http://adlnet.gov/expapi/verbs/initialized": require('../schemas/scorm.profile.initializing.attempt.schema'),
+    "http://adlnet.gov/expapi/verbs/terminated": require('../schemas/scorm.profile.terminating.attempt.schema.json'),
+    "http://adlnet.gov/expapi/verbs/resumed": require('../schemas/scorm.profile.resuming.attempt.schema.json'),
+    "http://adlnet.gov/expapi/verbs/suspended": require('../schemas/scorm.profile.suspending.attempt.schema.json')
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,9 +15,19 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-    debug(req.body);
+//    debug(req.body);
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify([req.body.id || 'nooooope']));
+    var schema = schemas[req.body.verb.id];
+    if (!schema) { 
+        res.send({result: "statement didn't match a schema.. unvalidated"});
+        return;
+    }
+    var validatedresponse = validate(req.body, schema);
+    if ( validatedresponse.errors.length > 0 ) {
+        res.send(JSON.stringify(validatedresponse.errors));
+    } else {
+        res.send({result: "OK", schema: schema.id});
+    }
 });
 
 module.exports = router;
